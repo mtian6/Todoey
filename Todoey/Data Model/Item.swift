a96e254ab8873a0adf778c228a79a1b5bebd2eb9
Menglin Tian <mtian@solstice.com>
2018-09-12 16:25:39 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Menglin Tian on 9/12/18.
+//  Copyright © 2018 Menglin Tian. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
